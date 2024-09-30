6bb6d6218837fe49720fb7f1065196aa719fd41a
offor <offorchukwuebuka8@gmail.com>
2024-09-30 14:07:36 -0500

    5 commit

--- app/utils/CustomModal.tsx
+++ app/utils/CustomModal.tsx
@@ -1,38 +1,31 @@
-import React, {FC} from 'react'
-import {Modal, Box} from "@mui/material";
+import React, { FC } from 'react';
+import { Modal, Box } from '@mui/material';
 
-
-
+// Define the type for `activeItem` based on the expected structure
+// Replace 'YourType' with the appropriate interface/type for activeItem
 type Props = {
-    open: boolean;
-    setOpen: (open: boolean) => void;
-    activeItem:  unknown;  // Use 'unknown' if you don't know the structure yet
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  activeItem: unknown;  // Use 'unknown' if you don't know the structure yet
+  component: React.ComponentType<{ setOpen: (open: boolean) => void; setRoute?: (route: string) => void }>;  // Define proper types for the props passed to component
+  setRoute?: (route: string) => void;
+};
 
-    component: unknown;  // Use 'unknown' if you don't know the structure yet
-
-    setRoute?: (route: string) => void;
-
-}
-
-
-
-
-const CustomModal: FC<Props> = ({open, setOpen, setRoute,component:Component}) => {
+const CustomModal: FC<Props> = ({ open, setOpen, setRoute, component: Component }) => {
   return (
-     <Modal
-     open={open}
-     onClose={() => setOpen(false)}
-     aria-labelledby="modal-modal-title"
-     aria-describedby="modal-modal-description"
-     > 
-        <Box
-          className="absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 w-[450px] bg-white dark:bg-slate-900 rounded=[8px] shadow p-4 outline-none" 
-        >
-            <Component setOpen={setOpen} setRoute={setRoute} />
-
-        </Box>
-        </Modal>
-  )
-}
+    <Modal
+      open={open}
+      onClose={() => setOpen(false)}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box
+        className="absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 w-[450px] bg-white dark:bg-slate-900 rounded=[8px] shadow p-4 outline-none"
+      >
+        <Component setOpen={setOpen} setRoute={setRoute} />
+      </Box>
+    </Modal>
+  );
+};
 
 export default CustomModal;
