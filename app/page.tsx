2f9d22fd0598554300940c35b90a69a0e047f2ea
offor <offorchukwuebuka8@gmail.com>
2024-09-30 14:06:08 -0500

    5 commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,34 @@
 'use client'
-import React, {FC, useState} from "react";
+import React, { FC, useState } from "react";
 import Heading from "./utils/Heading";
 import Header from "./components/Header";
- import Hero from "./components/Route/Hero";
-import { Props } from "next/script";
+import Hero from "./components/Route/Hero";
 
-
-
-const Page: FC<Props> = (props) => {
+// If Props from 'next/script' is unnecessary, you can remove it.
+// If you need Props in the future, define its type properly, otherwise, remove it.
+const Page: FC = () => {
   const [open, setOpen] = useState(false);
-  const [activeItem, setActiveItem] = useState(0);
+  // `setActiveItem` is currently unused, so you can either remove it or leave it for future use.
+  const [activeItem] = useState(0);
   const [route, setRoute] = useState("Login");
+
   return (
     <div>
-       <Heading
-       title="ELearning"
-       description="ELearning is a platform for students to learn and get help from teachers"
-       keywords="Programming,MERN,Redux,Machnine Learning"
-       />
-       <Header
+      <Heading
+        title="ELearning"
+        description="ELearning is a platform for students to learn and get help from teachers"
+        keywords="Programming,MERN,Redux,Machine Learning"
+      />
+      <Header
         open={open}
         setOpen={setOpen}
         activeItem={activeItem}
         setRoute={setRoute}
         route={route}
-       
-       />
-       <Hero/>  
-     
+      />
+      <Hero />
     </div>
-  )
-}
+  );
+};
 
 export default Page;
